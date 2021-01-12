caaa112cc4ba1b4882e18fc987dfdf24f111ccfa
Maddie Higgins <maddiehiggins9@gmail.com>
2021-01-12 13:49:44 -0600

    style: streamline layout - remove bootstrap artifacts

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -4,20 +4,16 @@ import Nav from "./Nav";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <div className="container">
-          <Nav />
-          <div className="navbar-header">
-            The Shoppies
-          </div>
-        </div>
-        </div>
-      <hr/>
-      <div className="container content">
+      <div className="container" role="navigation">
+        <Nav />
+        <h1 className="navbar-header">
+          The Shoppies
+        </h1>
+      </div>
+      <div className="container">
           {children}
       </div>
-      <hr/>
-      <div id="footer" className="container text-muted footer">
+      <div className="footer">
         App by Madeline Higgins
       </div>
     </>
